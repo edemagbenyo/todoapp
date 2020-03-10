08735f74db49442eb00a987371da5b2765bddedd
Edemone <edem.agbenyo@gmail.com>
2020-03-10 19:04:42 +0000

    Implement addition of todo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import Structure from './structure';
-
+import './style.css'
 const form = document.querySelector('form');
 const inputTitle = document.querySelector('#inputTodo');
 const txtDescription = document.querySelector('#txtDescription');
 const inputDate = document.querySelector('#inputDate');
 const seletPriority = document.querySelector('#seletPriority');
-const ulTodos = document.querySelector('#ulTodos');
+const todoList = document.querySelector('#todoList');
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
-  const li = document.createElement('li');
+  const divItem = document.createElement('div');
+  divItem.classList='item';
+ 
+
   // console.log(checkbox);
   const todo = Structure.addTodo(inputTitle.value,txtDescription.value,inputDate.value,seletPriority.value);
-   li.innerHTML = `<input type="checkbox"> ${todo.title} : ${todo.description}`;
+  divItem.innerHTML = `<input type="checkbox"><p>${todo.title} : ${todo.description}</p>`;
   
-  ulTodos.append(li);
+  todoList.append(divItem);
+
 })
