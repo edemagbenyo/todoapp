609c96eb4ebc06d789bf0a88b0d435fd7e42fd2e
Selma Datagirl <datagirlcmr@gmail.com>
2020-03-11 11:04:24 -0500

    styled the UI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import Structure from './structure';
 import './style.css'
+
+const button = document.querySelector('button');
 const form = document.querySelector('form');
 const inputTitle = document.querySelector('#inputTodo');
 const txtDescription = document.querySelector('#txtDescription');
 const inputDate = document.querySelector('#inputDate');
 const seletPriority = document.querySelector('#seletPriority');
 const todoList = document.querySelector('#todoList');
+
+button.addEventListener('click', () => {
+  button.style.display = "none";
+  form.style.display = "inline";
+})
+
+form.style.display = "none";
+
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
